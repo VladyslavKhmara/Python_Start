9a6b51aa9ee91e3e0a024d587e55471cf45fd617
Vladyslav Khmara <vladyslav.khmara@vladyslavs-macbook-pro.local>
2023-12-07 17:19:37 +0200

    Initial commit

---
+++ task_1.py
@@ -0,0 +1,29 @@
+print ("Hello world")
+
+print ("Python is","the best","programing language")
+
+x = input("x = ")
+y = input("y = ")
+S = int(x) * int(y)
+print(S)
+
+number_1 = input("Write first number = ")
+number_2 = input("Write second number = ")
+sum = int(number_1) + int(number_2)
+diff = int(number_1) - int(number_2)
+prod = int(number_1) * int(number_2)
+quot = int(number_1) / int(number_2)
+print("Sum is",str(sum))
+print("Product is",str(prod))
+print("Difference is",str(diff))
+print("Quotient is",str(quot))
+
+r = float(input("Write r = "))
+p = 3.14159
+d = r * float(2)
+C = float(2) * p * r
+S = float(p * r ** 2)
+
+print("Diameter: D = 2r =", d)
+print("Circumference: C = 2π⋅r =", C)
+print("Area: S = π⋅r2 =", S)
